072d85ab95a877a5791b39bd4c61e381ee151f47
Alex Milbert <alex.milbert@gmail.com>
2017-10-27 23:06:38 -0700

    App additional notes feature

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-
 export default class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props); //passing props to super by convention.  Otherwise this.props doesn't get set
 
-    this.state = {
+    this.state = { //App owns the State.  That is demonstrated here on this line.
       notes: [
         {
           id: uuid.v4(),
@@ -22,14 +21,29 @@ export default class App extends React.Component {
     }
   }
   render() {
-    const {notes}  = this.state;
+    const {notes}  = this.state;  // We set the state to {notes} and then it is returned and rendered six lines below
 
     return (
         <div>
-          <button onClick={() => console.log('add note')}>+</button>
+          <button onClick={this.addNote}>+</button>
+          <button onClick={this.deleteNote}>X</button>
           <input type='tel'></input>
-          <Notes notes={notes} />
+          <Notes notes={notes} /> // the rendering of notes which as described above, is the rendering of the current state
         </div>
     )
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New Task'
+      }])
+    });
+  }
+  deleteNote = () => {
+    this.setState({
+      return
+    })  
   }
 }
