12a56b65dd3a2dcc31886aefccfddfdba1987af3
Alex Milbert <alex.milbert@gmail.com>
2017-10-27 22:24:38 -0700

    App converted to class based comp

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,23 +2,34 @@ import React from 'react';
 
 import uuid from 'uuid';
 import Notes from './Notes';
-const notes = [
-{
-  // id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
-  id: uuid.v4(),
-  task: 'Learn React'
-},
-{
-  // id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
-  id: uuid.v4(),
-  task: 'Do laundry'
-}
-];
 
-export default () => (
-  <div>
-    <button onClick={() => console.log('add note')}>+</button>
-    <input type='tel'></input>
-    <Notes notes={notes} />
-  </div>
-);
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    }
+  }
+  render() {
+    const {notes}  = this.state;
+
+    return (
+        <div>
+          <button onClick={() => console.log('add note')}>+</button>
+          <input type='tel'></input>
+          <Notes notes={notes} />
+        </div>
+    )
+  }
+}
